34be63102ca99c77eeb8db3c867fbde3cfe8e7b2
Omekagu Michael <michaelomekagu@gmail.com>
2025-06-20 09:12:14 +0100

    confirmed initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function Main() {
+    return(
+        <div>
+            <h1> The main text goes here </h1>
+        </div>
+    )
+}
+
+export default Main
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+function Navbar() {
+    return(
+        <nav>
+            <h1> Navbar goes here. </h1>
+        </nav>
+    )
+}
+
+export default Navbar

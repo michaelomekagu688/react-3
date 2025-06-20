ad77bd209e7ab6cf5751b237e6e11fc1a714656c
Omekagu Michael <michaelomekagu@gmail.com>
2025-06-20 10:35:39 +0100

    worked on the project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 
 import React from 'react';
+import logoImg from "../images/logo192.png";
 
 function Navbar() {
     return(
-        <nav>
-            <h1> Navbar goes here. </h1>
+        <nav className='nav-main'> 
+            <img src={logoImg} alt='logo' className='logo-img' />
+            <h1 className='nav-h1'> ReactFacts</h1>
+            <h4> ReactCourse - Project 1</h4>
         </nav>
     )
 }
